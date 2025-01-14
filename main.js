5c3d509bafdf31c0ade280ca15093f32d1c40637
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-14 17:57:53 +0100

    Mail completed

--- main.js
+++ main.js
@@ -8,8 +8,61 @@ Non è consentito usare nessun metodo proprio degli array (come includes, per es
 Si può fare? Certo che si basta ragionare un po’.
 Nota:
 Non è necessario provvedere alla validazione delle email
+*/
+/*
+    MINI TASK
+    -  Crea un array con le mail della lista degli invitati
+    -  Chiedi all'utente di inserire la sua email con la funzione prompt
+    -  Crea  una variabile per il controllo di accesso con valore booleano  false/o
+    -  Utilizza un ciclo for per iterare attraverso l'array listaInvitati
+    -
+*/
+
+// Lista delle email degli invitati
+const listaInvitati = [
+    "mario@example.com",
+    "lucia@example.com",
+    "giorgio@example.com",
+    "anna@example.com",
+    "paola@example.com"
+];
+
+// Chiediamo all'utente la sua email
+let emailUtente = prompt("Inserisci la tua email:");
+
+// Variabile di  verifica con valore 0
+let accessoConcesso = false;
+
+// Ciclo attraverso la lista di email
+for (let i = 0; i < listaInvitati.length; i++) {
+    // Se troviamo una corrispondenza
+    if (listaInvitati[i] === emailUtente) {
+        accessoConcesso = true;
+        break; // Esci dal ciclo, non è necessario continuare
+    }
+};
+
+// Messaggio appropriato in base al risultato
+if (accessoConcesso) {
+    alert("Accesso consentito! Benvenuto alla festa.");
+} else {
+    alert("Mi dispiace, non sei nella lista degli invitati.");
+};
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 Gioco dei dadi:
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
